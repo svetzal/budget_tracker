04fc77a02ebd45d7c2bde8e42940a0c39aaa46d8
Stacey Vetzal <stacey@vetzal.com>
2023-07-29 08:55:48 -0400

    fixing menu up a bit

--- practice_menu.py
+++ practice_menu.py
@@ -1,7 +1,9 @@
 import inspect
+from typing import List
 
 from entities import CoachingPracticeFinance
 from presentation import Colours
+from use_cases.add_area_assignment import AddAreaAssignment
 from use_cases.report_next_fiscal_year_monthly import NextFiscalYearBudgetMonthly
 from use_cases.report_current_fiscal_year_monthly import CurrentFiscalYearMonthly
 from use_cases.list_consultancies import ListConsultancies
@@ -15,40 +17,44 @@ from use_cases.add_consultancy import AddConsultancy
 def main():
     practice = CoachingPracticeFinance.load()
 
-    functions = {
-        'add_consultancy': {
+    functions: List[dict] = [
+        {
+            'func': AddAreaAssignment(practice).execute,
+            'label': 'Add Area Assignment'
+        },
+        {
             'func': AddConsultancy(practice).execute,
             'label': 'Add Consultancy'
         },
-        'list_consultancies': {
+        {
             'func': ListConsultancies(practice).execute,
             'label': 'List Consultancies'
         },
-        'add_contractor': {
+        {
             'func': AddContractor(practice).execute,
             'label': 'Add Contractor'
         },
-        'add_employee': {
+        {
             'func': AddEmployee(practice).execute,
             'label': 'Add Employee'
         },
-        'list_people': {
+        {
             'func': ListPeople(practice).execute,
             'label': 'List People'
         },
-        'add_transaction_agreement': {
+        {
             'func': AddTransactionAgreement(practice).execute,
             'label': 'Add Transaction Agreement'
         },
-        'current_fiscal_year_monthly': {
+        {
             'func': CurrentFiscalYearMonthly(practice).execute,
             'label': 'Current Fiscal Year Monthly'
         },
-        'next_fiscal_year_budget_monthly': {
+        {
             'func': NextFiscalYearBudgetMonthly(practice).execute,
             'label': 'Next Fiscal Year Budget Monthly'
-        }
-    }
+        },
+    ]
 
     while True:
         print("")
@@ -57,8 +63,8 @@ def main():
         print("Menu:")
         print(f"{Colours.OKGREEN}-1{Colours.RESET} - Exit Without Saving")
         print(f" {Colours.OKGREEN}0{Colours.RESET} - Save & Exit")
-        for i, key in enumerate(functions.keys()):
-            print(f" {Colours.OKGREEN}{i + 1}{Colours.RESET} - {functions[key]['label']}")
+        for i, function in enumerate(functions):
+            print(f" {Colours.OKGREEN}{i + 1}{Colours.RESET} - {function['label']}")
         print("")
 
         choice = int(input("Enter choice: "))
@@ -70,7 +76,7 @@ def main():
             practice.save()
             exit()
 
-        chosen_func = list(functions.values())[choice - 1]['func']
+        chosen_func = functions[choice-1]['func']
         argspec = inspect.getfullargspec(chosen_func)
         args = {}
         for i, arg in enumerate(argspec.args):
