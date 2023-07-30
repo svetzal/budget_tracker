68036420e69a12b2ca5132c10cc9cbdc9404fc90
Stacey Vetzal <stacey@vetzal.com>
2023-07-30 15:43:17 -0400

    fixing optionality

--- use_cases/add_consultancy.py
+++ use_cases/add_consultancy.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from entities import Consultancy
 from use_cases.use_case import UseCase
 
 
 class AddConsultancy(UseCase):
-    def execute(self, code: str, name: str, contract: str, contact_name: str, contact_phone: str, contact_email: str):
+    def execute(self, code: str, name: str, contract: str, contact_name: str, contact_phone: Optional[str] = None,
+                contact_email: Optional[str] = None):
         self.guard_consultancy_not_duplicate(code)
         consultancy = Consultancy(
             code=code,
