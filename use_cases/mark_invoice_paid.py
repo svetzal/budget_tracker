9a4592e67bb82b4aa175ac44a64198559d34ce90
Stacey Vetzal <stacey@vetzal.com>
2023-07-30 15:35:33 -0400

    fixing up logic issue

--- use_cases/mark_invoice_paid.py
+++ use_cases/mark_invoice_paid.py
@@ -4,6 +4,8 @@ from use_cases.use_case import UseCase
 
 
 class MarkInvoiceAsPaid(UseCase):
-    def execute(self, invoice_number: str, paid_date: str):
-        invoice = self.find_invoice(invoice_number)
-        invoice.paid_date = date.fromisoformat(paid_date)
+    def execute(self, consultancy: str, invoice_number: str, paid_date: str):
+        for c in self.practice.consultancies:
+            for i in c.invoices:
+                if c.code == consultancy and i.number == invoice_number:
+                    i.paid_date = date.fromisoformat(paid_date)
