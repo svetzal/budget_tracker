6eb8a9dc003a496dd750d3cfd17dcacf3049f43d
Stacey Vetzal <stacey@vetzal.com>
2023-07-30 15:36:06 -0400

    fixing up logic issue

--- use_cases/mark_invoice_paid_specs.py
+++ use_cases/mark_invoice_paid_specs.py
@@ -2,7 +2,7 @@ from entities import CoachingPracticeFinance
 from use_cases.add_expense_line_item_to_invoice import AddExpenseLineItemToInvoice
 from use_cases.add_invoice import AddInvoice
 from use_cases.mark_invoice_paid import MarkInvoiceAsPaid
-from use_cases.test_data_generators import FIRST_INVOICE_NUMBER
+from use_cases.test_data_generators import FIRST_INVOICE_NUMBER, FIRST_CONSULTANCY_CODE
 from verifiers import verify_model
 
 
@@ -11,6 +11,6 @@ def test_can_mark_invoice_paid(staffed_practice: CoachingPracticeFinance, first_
     AddInvoice(staffed_practice).execute(**first_invoice_details)
     AddExpenseLineItemToInvoice(staffed_practice).execute(**first_expense_details)
 
-    MarkInvoiceAsPaid(staffed_practice).execute(FIRST_INVOICE_NUMBER, "2023-02-06")
+    MarkInvoiceAsPaid(staffed_practice).execute(FIRST_CONSULTANCY_CODE, FIRST_INVOICE_NUMBER, "2023-02-06")
 
     verify_model(staffed_practice.consultancies)
