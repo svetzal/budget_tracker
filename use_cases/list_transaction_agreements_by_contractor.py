3ad58a36b77b2806b288d2e49172a5ccb7d2467e
Stacey Vetzal <stacey@vetzal.com>
2023-07-30 16:33:10 -0400

    bug

--- use_cases/list_transaction_agreements_by_contractor.py
+++ use_cases/list_transaction_agreements_by_contractor.py
@@ -10,6 +10,7 @@ class ListTransactionAgreementsByContractor(UseCase):
         rows = []
         for c in self.practice.contractors:
             for t in self.practice.transaction_agreements:
-                rows.append([c.name, t.number, t.hours, t.rate, t.start_date, t.end_date])
+                if t.contractor_code == c.code:
+                    rows.append([c.name, t.number, t.hours, t.rate, t.start_date, t.end_date])
         df = pd.DataFrame(rows, columns=["Contractor", "Number", "Hours", "Rate", "Start", "End"])
         return DataFrameRenderer(title="Transaction Agreements", data=df, sort_by=["Contractor"]).lines()
