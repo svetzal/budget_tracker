208ff915f98e86791845dad13e058c6c3cd3e52a
Stacey Vetzal <stacey@vetzal.com>
2023-08-01 12:27:52 -0400

    fixing up report use case

--- use_cases/list_unstaffed_areas.py
+++ use_cases/list_unstaffed_areas.py
@@ -20,13 +20,13 @@ class ListUnstaffedAreas(UseCase):
 
     def prepare_report_output(self, report_frame):
         lines = []
-        for a in self.practice.area_assignments:
-            unstaffed = report_frame[report_frame[a.support_area_code] == False]
-            report = unstaffed.groupby(f"{a.support_area_code}_group")['date'].agg(['min', 'max'])
+        for a in self.practice.support_areas:
+            unstaffed = report_frame[report_frame[a.code] == False]
+            report = unstaffed.groupby(f"{a.code}_group")['date'].agg(['min', 'max'])
             report = report.reset_index(drop=True)
             if report.count()[0] > 0:
                 lines.append("")
-                lines.append(f"{self.find_support_area(a.support_area_code).name} is Unstaffed")
+                lines.append(f"{self.find_support_area(a.code).name} is Unstaffed")
                 lines.append("")
                 report['min'] = report['min'].dt.strftime('%Y-%m-%d')
                 report['max'] = report['max'].dt.strftime('%Y-%m-%d')
@@ -35,18 +35,23 @@ class ListUnstaffedAreas(UseCase):
         return lines
 
     def prepare_date_groupings(self, report_frame):
-        for a in self.practice.area_assignments:
-            report_frame[f"{a.support_area_code}_group"] = (report_frame[a.support_area_code].diff() != 0).cumsum()
+        for a in self.practice.support_areas:
+            report_frame[f"{a.code}_group"] = (report_frame[a.code].diff() != 0).cumsum()
 
     def prepare_assignments_frame(self, report_date_range):
         assignment_coverage = []
         columns = ['date'] + [a.support_area_code for a in self.practice.area_assignments]
         for date in report_date_range:
             row = [date]
-            for a in self.practice.area_assignments:
-                start = pd.to_datetime(a.start_date)
-                end = pd.to_datetime(a.end_date)
-                row.append(start <= date and end >= date)
+            for a in self.practice.support_areas:
+                covered = False
+                for aa in self.practice.area_assignments:
+                    if aa.support_area_code == a.code:
+                        start = pd.to_datetime(aa.start_date)
+                        end = pd.to_datetime(aa.end_date)
+                        if start <= date <= end:
+                            covered = True
+                row.append(covered)
             assignment_coverage.append(row)
         assignments_frame = pd.DataFrame(assignment_coverage, columns=columns)
         return assignments_frame
