7d6d508fe9e865a37d832c976954ed3d735af3e0
Stacey Vetzal <stacey@vetzal.com>
2023-08-25 05:39:58 -0400

    adding the ability to build into a mac app

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    # 'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleName': 'Coaching Practice Finance',
+        'CFBundleDisplayName': 'Coaching Practice Finance',
+        'CFBundleGetInfoString': "Coaching Practice Finance",
+        'CFBundleIdentifier': "com.example.coaching-practice-finance",
+        'CFBundleVersion': "0.1.0",
+        'CFBundleShortVersionString': "0.1.0",
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
